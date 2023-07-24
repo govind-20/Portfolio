4fbfad154b32b5b7d54c0e75b326d50256b17339
Govind Sharma <84737099+govind-20@users.noreply.github.com>
2023-07-24 23:47:10 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+document.getElementById('hire').addEventListener('click',function(){
+    const mail='govindsharma9382@gmail.com';
+    const mailto=`mailto:${mail}`;
+    window.location.href=mailto;
+})
+
+document.getElementById('mail').addEventListener('click',function(){
+    const mail1='govindsharma9382@gmail.com';
+    const mailto=`mailto:${mail1}`;
+    window.location.href=mailto;
+})
 
